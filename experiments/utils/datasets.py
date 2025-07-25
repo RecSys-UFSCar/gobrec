345868d50a98b2477a2e323b3b21c0ad0d1d19aa
GregorioFornetti <fornetti.greg@gmail.com>
2025-07-25 20:17:03 +0000

    more toy datasets

--- experiments/utils/datasets.py
+++ experiments/utils/datasets.py
@@ -6,17 +6,25 @@ from utils.generate_toy_datasets import load_or_generate_toy_dataset
 from utils.constants import TOY_DATASETS_SAVE_PATH
 
 DATASETS_TABLE = pd.DataFrame(
-    [[1,     'toy_dataset_1k_1M',      lambda: load_or_generate_toy_dataset(1_000,   1_000_000,  os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_1k_1M'))],
-     [2,     'toy_dataset_1k_5M',      lambda: load_or_generate_toy_dataset(1_000,   5_000_000,  os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_1k_5M'))],
-     [3,     'toy_dataset_1k_15M',     lambda: load_or_generate_toy_dataset(1_000,   15_000_000, os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_1k_15M'))],
-     [4,     'toy_dataset_1k_30M',     lambda: load_or_generate_toy_dataset(1_000,   30_000_000, os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_1k_30M'))],
-     [5,     'toy_dataset_10k_1M',     lambda: load_or_generate_toy_dataset(10_000,  1_000_000,  os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_10k_1M'))],
-     [6,     'toy_dataset_10k_5M',     lambda: load_or_generate_toy_dataset(10_000,  5_000_000,  os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_10k_5M'))],
-     [7,     'toy_dataset_10k_15M',    lambda: load_or_generate_toy_dataset(10_000,  15_000_000, os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_10k_15M'))],
-     [8,     'toy_dataset_10k_30M',    lambda: load_or_generate_toy_dataset(10_000,  30_000_000, os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_10k_30M'))],
-     [9,     'toy_dataset_100k_1M',    lambda: load_or_generate_toy_dataset(100_000, 1_000_000,  os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_100k_1M'))],
-     [10,    'toy_dataset_100k_5M',    lambda: load_or_generate_toy_dataset(100_000, 5_000_000,  os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_100k_5M'))],
-     [11,    'toy_dataset_100k_15M',   lambda: load_or_generate_toy_dataset(100_000, 15_000_000, os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_100k_15M'))],
-     [12,    'toy_dataset_100k_30M',   lambda: load_or_generate_toy_dataset(100_000, 30_000_000, os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_100k_30M'))]],
+    [
+     [1,     'toy_dataset_1k_100k',    lambda: load_or_generate_toy_dataset(1_000,   100_000,    os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_1k_100k'))],
+     [2,     'toy_dataset_1k_500k',    lambda: load_or_generate_toy_dataset(1_000,   500_000,    os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_1k_500k'))],
+     [3,     'toy_dataset_1k_1M',      lambda: load_or_generate_toy_dataset(1_000,   1_000_000,  os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_1k_1M'))],
+     [4,     'toy_dataset_1k_5M',      lambda: load_or_generate_toy_dataset(1_000,   5_000_000,  os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_1k_5M'))],
+     [5,     'toy_dataset_1k_15M',     lambda: load_or_generate_toy_dataset(1_000,   15_000_000, os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_1k_15M'))],
+     [6,     'toy_dataset_1k_30M',     lambda: load_or_generate_toy_dataset(1_000,   30_000_000, os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_1k_30M'))],
+     
+     [7,     'toy_dataset_10k_100k',   lambda: load_or_generate_toy_dataset(10_000,  100_000,    os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_10k_100k'))],
+     [8,     'toy_dataset_10k_500k',   lambda: load_or_generate_toy_dataset(10_000,  500_000,    os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_10k_500k'))],
+     [9,     'toy_dataset_10k_1M',     lambda: load_or_generate_toy_dataset(10_000,  1_000_000,  os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_10k_1M'))],
+     [10,    'toy_dataset_10k_5M',     lambda: load_or_generate_toy_dataset(10_000,  5_000_000,  os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_10k_5M'))],
+     [11,    'toy_dataset_10k_15M',    lambda: load_or_generate_toy_dataset(10_000,  15_000_000, os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_10k_15M'))],
+     [12,    'toy_dataset_10k_30M',    lambda: load_or_generate_toy_dataset(10_000,  30_000_000, os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_10k_30M'))],
+     
+     [13,    'toy_dataset_100k_500k',  lambda: load_or_generate_toy_dataset(100_000, 500_000,    os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_100k_500k'))],
+     [14,    'toy_dataset_100k_1M',    lambda: load_or_generate_toy_dataset(100_000, 1_000_000,  os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_100k_1M'))],
+     [15,    'toy_dataset_100k_5M',    lambda: load_or_generate_toy_dataset(100_000, 5_000_000,  os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_100k_5M'))],
+     [16,    'toy_dataset_100k_15M',   lambda: load_or_generate_toy_dataset(100_000, 15_000_000, os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_100k_15M'))],
+     [17,    'toy_dataset_100k_30M',   lambda: load_or_generate_toy_dataset(100_000, 30_000_000, os.path.join(TOY_DATASETS_SAVE_PATH, 'toy_dataset_100k_30M'))]],
     columns=['id', 'name', 'get_dataset']
 ).set_index('id')
