7d0f0a5eacea4615e90cae6d9af04a728fdca378
GregorioFornetti <fornetti.greg@gmail.com>
2025-07-25 15:32:02 +0000

    fix dataset import

--- experiments/utils/datasets.py
+++ experiments/utils/datasets.py
@@ -1,6 +1,6 @@
 
 import pandas as pd
-from generate_toy_datasets import load_or_generate_toy_dataset
+from utils.generate_toy_datasets import load_or_generate_toy_dataset
 
 DATASETS_TABLE = pd.DataFrame(
     [[1,     'toy_dataset_1k_1M',      lambda: load_or_generate_toy_dataset(1_000,   1_000_000,  'toy_datasets/toy_dataset_1k_1M')],
