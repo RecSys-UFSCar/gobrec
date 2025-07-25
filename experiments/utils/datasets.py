baa5a1a2be4bea37d145e14129e2fcb08f617cb4
GregorioFornetti <fornetti.greg@gmail.com>
2025-07-25 15:08:32 +0000

    datasets table

---
+++ experiments/utils/datasets.py
@@ -0,0 +1,19 @@
+
+import pandas as pd
+from generate_toy_datasets import load_or_generate_toy_dataset
+
+DATASETS_TABLE = pd.DataFrame(
+    [[1,     'toy_dataset_1k_1M',      lambda: load_or_generate_toy_dataset(1_000,   1_000_000,  'toy_datasets/toy_dataset_1k_1M')],
+     [2,     'toy_dataset_1k_5M',      lambda: load_or_generate_toy_dataset(1_000,   5_000_000,  'toy_datasets/toy_dataset_1k_5M')],
+     [3,     'toy_dataset_1k_15M',     lambda: load_or_generate_toy_dataset(1_000,   15_000_000, 'toy_datasets/toy_dataset_1k_15M')],
+     [4,     'toy_dataset_1k_30M',     lambda: load_or_generate_toy_dataset(1_000,   30_000_000, 'toy_datasets/toy_dataset_1k_30M')],
+     [5,     'toy_dataset_10k_1M',     lambda: load_or_generate_toy_dataset(10_000,  1_000_000,  'toy_datasets/toy_dataset_10k_1M')],
+     [6,     'toy_dataset_10k_5M',     lambda: load_or_generate_toy_dataset(10_000,  5_000_000,  'toy_datasets/toy_dataset_10k_5M')],
+     [7,     'toy_dataset_10k_15M',    lambda: load_or_generate_toy_dataset(10_000,  15_000_000, 'toy_datasets/toy_dataset_10k_15M')],
+     [8,     'toy_dataset_10k_30M',    lambda: load_or_generate_toy_dataset(10_000,  30_000_000, 'toy_datasets/toy_dataset_10k_30M')],
+     [9,     'toy_dataset_100k_1M',    lambda: load_or_generate_toy_dataset(100_000, 1_000_000,  'toy_datasets/toy_dataset_100k_1M')],
+     [10,    'toy_dataset_100k_5M',    lambda: load_or_generate_toy_dataset(100_000, 5_000_000,  'toy_datasets/toy_dataset_100k_5M')],
+     [11,    'toy_dataset_100k_15M',   lambda: load_or_generate_toy_dataset(100_000, 15_000_000, 'toy_datasets/toy_dataset_100k_15M')],
+     [12,    'toy_dataset_100k_30M',   lambda: load_or_generate_toy_dataset(100_000, 30_000_000, 'toy_datasets/toy_dataset_100k_30M')]],
+    columns=['id', 'name', 'get_dataset']
+).set_index('id')
