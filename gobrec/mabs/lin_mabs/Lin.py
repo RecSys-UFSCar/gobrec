f4d917bf74e57f8e545f6138b84b88b9ce0325f0
GregorioFornetti <fornetti.greg@gmail.com>
2025-08-22 09:38:16 +0000

    create MABAlgo class

--- gobrec/mabs/lin_mabs/Lin.py
+++ gobrec/mabs/lin_mabs/Lin.py
@@ -2,49 +2,42 @@
 import torch
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
+from gobrec.mabs.MABAlgo import MABAlgo
 
 
-class Lin:
+class Lin(MABAlgo):
 
     def __init__(self, seed: int = None, l2_lambda: float = 1.0, use_gpu: bool = False):
+        
+        super().__init__(seed)
 
-        self.seed = seed
-        self.rng = np.random.default_rng(self.seed)
         self.l2_lambda = l2_lambda
         self.device = 'cuda' if use_gpu else 'cpu'
 
-        self.label_encoder = None
-        self.num_features = None
         self.items_per_batch = 1000
+        self.already_initialized = False
     
     def _update_label_encoder_and_matrices_sizes(self, items_ids: np.ndarray, num_features: int):
-        if self.label_encoder is None:
-            self.label_encoder = LabelEncoder()
-            self.label_encoder.fit(items_ids)
-            num_arms = len(self.label_encoder.classes_)
-            self.num_features = num_features
-            self.Xty = torch.zeros((num_arms, num_features), device=self.device, dtype=torch.double)
-            self.A = torch.eye(num_features, device=self.device, dtype=torch.double).unsqueeze(0).repeat(num_arms, 1, 1) * self.l2_lambda
-            self.beta = torch.zeros((num_arms, num_features), device=self.device, dtype=torch.double)
-        else:
-            assert num_features == self.num_features, "Number of features has changed!"
-            new_classes = np.setdiff1d(items_ids, self.label_encoder.classes_)
-            if len(new_classes) > 0:
-                all_classes = np.concatenate((self.label_encoder.classes_, new_classes))
-                self.label_encoder.fit(all_classes)
-                num_arms = len(self.label_encoder.classes_)
 
-                Xty_new = torch.zeros((num_arms, num_features), device=self.device, dtype=torch.double)
-                Xty_new[:self.Xty.shape[0]] = self.Xty
-                self.Xty = Xty_new
+        self._update_label_encoder(items_ids, num_features)
 
-                A_new = torch.eye(num_features, device=self.device, dtype=torch.double).unsqueeze(0).repeat(num_arms, 1, 1) * self.l2_lambda
-                A_new[:self.A.shape[0]] = self.A
-                self.A = A_new
+        if not self.already_initialized:
+            self.Xty = torch.zeros((self.num_arms, self.num_features), device=self.device, dtype=torch.double)
+            self.A = torch.eye(self.num_features, device=self.device, dtype=torch.double).unsqueeze(0).repeat(self.num_arms, 1, 1) * self.l2_lambda
+            self.beta = torch.zeros((self.num_arms, self.num_features), device=self.device, dtype=torch.double)
+        elif self.num_arms != self.beta.shape[0]:
+            # the number of arms has changed, we need to update the matrices
+            Xty_new = torch.zeros((self.num_arms, self.num_features), device=self.device, dtype=torch.double)
+            Xty_new[:self.Xty.shape[0]] = self.Xty
+            self.Xty = Xty_new
 
-                beta_new = torch.zeros((num_arms, num_features), device=self.device, dtype=torch.double)
-                beta_new[:self.beta.shape[0]] = self.beta
-                self.beta = beta_new
+            A_new = torch.eye(self.num_features, device=self.device, dtype=torch.double).unsqueeze(0).repeat(self.num_arms, 1, 1) * self.l2_lambda
+            A_new[:self.A.shape[0]] = self.A
+            self.A = A_new
+
+            beta_new = torch.zeros((self.num_arms, self.num_features), device=self.device, dtype=torch.double)
+            beta_new[:self.beta.shape[0]] = self.beta
+            self.beta = beta_new
 
 
     def fit(self, contexts: np.ndarray, items_ids: np.ndarray, rewards: np.ndarray):
